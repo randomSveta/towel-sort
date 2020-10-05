17326946d1729e9df7c531759ab26f16c24aee52
Sveta K <test@test.test>
2020-10-05 11:45:28 +0100

    init: start and finish writing in towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    let sortedArray = [];
+    if (Array.isArray(matrix)) {
+        sortedArray = matrix.map((item, index) => {
+            if (index % 2) return item.sort((a, b) => b - a)
+            else return item.sort((a, b) => a - b)
+        });
+    }
+    return sortedArray.flat();
 }
